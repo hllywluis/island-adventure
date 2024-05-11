09225f7576165529673dcc799cd5d28559af1687
Luis Bauza <hllywluis2@me.com>
2024-05-11 14:21:25 -0400

    Modify some files

--- source/main.lua
+++ source/main.lua
@@ -1,16 +1,4 @@
+import "CoreLibs/object"
 import "CoreLibs/graphics"
 
-local pd <const> = playdate
-local gfx <const> = pd.graphics
-
-local playerX, playerY = 200, 120
-local playerRadius = 10
-local playerSpeed = 3
-
-function pd.update()
-    gfx.clear()
-    local crankAngle = math.rad(pd.getCrankPosition())
-    playerX = playerX + math.sin(crankAngle) * playerSpeed
-    playerY = playerY - math.cos(crankAngle) * playerSpeed
-    gfx.fillCircleAtPoint(playerX, playerY, playerRadius)
-end
+import "../toyboxes/toyboxes.lua"
