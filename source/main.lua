cc1ac7a201d561f71557abd49749e6dc3f5adc15
Luis Bauza <hllywluis2@me.com>
2024-05-11 19:05:17 -0400

    Very barebones "menu"

--- source/main.lua
+++ source/main.lua
@@ -1,4 +1,13 @@
 import "CoreLibs/object"
 import "CoreLibs/graphics"
+import "CoreLibs/sprites"
 
 import "../toyboxes/toyboxes.lua"
+import "menus/main_menu"
+
+MainMenu(200, 120, 400, 240)
+
+function playdate.update()
+    playdate.graphics.clear()
+    playdate.graphics.sprite.update()
+end
